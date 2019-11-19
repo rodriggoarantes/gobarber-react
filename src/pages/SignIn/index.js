01a50b10d7feed83892054fdb70916126254932c
rodrigo <rodriggoarantes@gmail.com>
2019-11-18 22:58:49 -0300

    #gobarber #3 estilizando tela de login e cadastro

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Informe um e-mail válido')
+    .required('Email é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit} schema={schema}>
+        <Input type="texemail" name="email" placeholder="Seu e-mail" />
+        <Input type="password" name="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+import logo from '~/assets/logo.svg';
 
-export default function SignUp() {
-  return <h1>SignUp</h1>;
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Informe um e-mail válido')
+    .required('Email é obrigatório'),
+  name: Yup.string().required('O nome é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
+
+export default function SignUn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit} schema={schema}>
+        <Input type="text" name="name" placeholder="Nome completo" />
+        <Input type="email" name="email" placeholder="Seu e-mail" />
+        <Input type="password" name="password" placeholder="Sua senha" />
+
+        <button type="submit">Registrar</button>
+
+        <Link to="/">Já tenho uma conta</Link>
+      </Form>
+    </>
+  );
 }
